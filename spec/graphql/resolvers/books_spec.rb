fa3ca5f020f1702341c202d216f03b75d91511c6
Vinícius Bispo <vbispo@vmox.com>
2023-10-22 10:55:20 -0300

    test(graphql): implement basic books resolver

--- spec/graphql/resolvers/books_spec.rb
+++ spec/graphql/resolvers/books_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe "POST /graphql books" do
   end
   describe '#books' do
     it 'returns only id of books' do
+      book = FactoryBot.create(:book)
+
       query_string = <<-GRAPHQL
         query {
           books {
@@ -14,9 +16,34 @@ RSpec.describe "POST /graphql books" do
         }
       GRAPHQL
 
-      result = graphql!(query_string:)
+      response = graphql!(query_string:)
 
-      expect(result['data']['books']).to be_an(Array)
+      result = response['data']['books'].map { _1["id"] }
+
+      expect(response['data']['books']).to be_an(Array)
+
+      expect(result).to eq([book.id.to_s])
+    end
+
+    it 'returns only title, and author' do
+      books = FactoryBot.create_list(:book, 2)
+
+      query_string = <<-GRAPHQL
+        query {
+          books {
+            title
+            author
+          }
+        }
+      GRAPHQL
+
+      response = graphql!(query_string:)
+
+      expected = books.map { {title: _1.title, author: _1.author }}
+
+      result = response['data']['books'].map { { title: _1["title"], author: _1["author"] }}
+
+      expect(result).to eq(expected)
     end
   end
 end
