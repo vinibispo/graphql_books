06b2e42553ba23cdb18c8fc2381e569181e06572
Vinícius Bispo <vbispo@vmox.com>
2023-10-22 10:53:05 -0300

    fix(factory_bot): add missing field published_at

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     title { Faker::Book.title }
     author { Faker::Book.author }
     description { Faker::Movie.quote }
+    published_at { rand(Date.civil(1800, 1, 1)..Date.civil(2000, 12, 31)) }
   end
 end
